a7679239b390099899e0f83605183990adc4c3e1
Max <bazuka5801@mail.ru>
2020-09-21 07:14:07 +0300

    feat: example implementation

--- example/android/app/src/main/java/com/bazumax/capacitor/vkauth/test/MainActivity.java
+++ example/android/app/src/main/java/com/bazumax/capacitor/vkauth/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.bazumax.capacitor.vkauth.test;
 
 import android.os.Bundle;
 
+import com.bazumax.plugins.vk.VKAuth;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
@@ -16,6 +17,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+      add(VKAuth.class);
     }});
   }
 }
