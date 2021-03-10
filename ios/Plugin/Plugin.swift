da8cc3610f3cfaa4aa1a872cf2895a2b378b55fa
Max <bazuka5801@mail.ru>
2021-03-10 20:43:48 +0300

    chore(ios): support Capacitor v3.rc0

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,7 +9,7 @@ import VK_ios_sdk
 @objc(VKAuth)
 public class VKAuth: CAPPlugin, VKSdkDelegate, VKSdkUIDelegate {
     public func vkSdkShouldPresent(_ controller: UIViewController!) {
-        if (self.bridge?.viewController?.presentedViewController != nil) {
+        if self.bridge?.viewController!.presentedViewController != nil {
             self.bridge?.viewController!.dismiss(animated: true, completion: {
                 self.bridge?.viewController!.present(controller, animated: true, completion: {
                 })
@@ -19,30 +19,29 @@ public class VKAuth: CAPPlugin, VKSdkDelegate, VKSdkUIDelegate {
             })
         }
     }
-    
+
     public func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
         print("vkSdkNeedCaptchaEnter")
     }
-    
-    
+
     public func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
         var resultData: [String: Any] = [:]
-        if (result.error != nil) {
+        if result.error != nil {
             resultData = [
                 "result": false,
                 "error": result.description
             ]
             return
-        } else if (result.token != nil) {
+        } else if result.token != nil {
             let token = result.token!
             resultData = [
                 "result": true,
                 "token": token.accessToken as Any,
                 "created": token.created,
                 "expiresIn": token.expiresIn,
-                "userId": token.userId as Any,
+                "userId": token.userId as Any
             ]
-            if (result.token.email != nil) {
+            if result.token.email != nil {
                 resultData.updateValue(result.token.email!, forKey: "email")
             }
         }
@@ -51,19 +50,17 @@ public class VKAuth: CAPPlugin, VKSdkDelegate, VKSdkUIDelegate {
             self.notifyListeners("vkAuthFinished", data: resultData)
         }
     }
-    
+
     public func vkSdkUserAuthorizationFailed() {
         self.notifyListeners("vkAuthFinished", data: [
             "result": false,
             "message": "UserAuthenticationFailed"
         ])
     }
-    
-    
-    var VK_APP_ID   : String = "123"
-    var sdkInstance : VKSdk? = nil
-    
-    
+
+    var VK_APP_ID: String = "123"
+    var sdkInstance: VKSdk?
+
     @objc func initWithId(_ call: CAPPluginCall) {
         VK_APP_ID = call.getString("id") ?? "123456"
         sdkInstance = VKSdk.initialize(withAppId: VK_APP_ID)
@@ -73,41 +70,38 @@ public class VKAuth: CAPPlugin, VKSdkDelegate, VKSdkUIDelegate {
         NotificationCenter.default.addObserver(
             self,
             selector: #selector(self.onCustomUrlOpen),
-            name: Notification.Name(CAPNotifications.URLOpen.name()),
+            name: Notification.Name.capacitorOpenURL,
             object: nil)
-        
-        call.success([
+
+        call.resolve([
             "message": "Success init with vk\(VK_APP_ID)"
         ])
     }
-    
-    
+
     @objc func auth(_ call: CAPPluginCall) {
         if sdkInstance == nil {
-            call.error("SDK is not initialized, please Initialize plugin before call auth method!")
+            call.reject("SDK is not initialized, please Initialize plugin before call auth method!")
             return
         }
-        
+
         let scope  = call.getArray("scope", String.self) ?? ["offline", "email"]
-        VKSdk.wakeUpSession(scope) { (state, error) in
-            if (state == VKAuthorizationState.authorized) {
-                VKSdk.forceLogout();
+        VKSdk.wakeUpSession(scope) { (state, _) in
+            if state == VKAuthorizationState.authorized {
+                VKSdk.forceLogout()
             }
-            
+
             DispatchQueue.main.async {
                 VKSdk.authorize(scope)
             }
-            
-            call.success([
+
+            call.resolve([
                 "message": "Success auth request"
             ])
         }
     }
-    
-    
-    
-    @objc private func onCustomUrlOpen(notification: NSNotification){
-        guard let object = notification.object as? [String:Any?] else {
+
+    @objc private func onCustomUrlOpen(notification: NSNotification) {
+        guard let object = notification.object as? [String: Any?] else {
             return
         }
 
